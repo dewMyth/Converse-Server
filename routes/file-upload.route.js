dd03f858cce5ca27edc2f3849e807b93722e0d43
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-12-18 12:48:21 +0530

    Add replaces to firebase private key

--- routes/file-upload.route.js
+++ routes/file-upload.route.js
@@ -9,7 +9,7 @@ const storage = FirebaseStorage({
   bucketName: process.env.FIREBASE_BUCKET_NAME,
   credentials: {
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
     projectId: process.env.FIREBASE_PROJECT_ID,
   },
   unique: true,
