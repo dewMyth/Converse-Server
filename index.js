9da0bff32381543886271122ffe0a3ac25dfd196
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-12-20 08:36:32 +0530

    Add socket configuration

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 // Use CORS to allow requests from any origin
 app.use(cors());
 
+const server = require("http").createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
 // Importing the required routes
 const userRoute = require("./routes/user.route");
 const fileUploadRoute = require("./routes/file-upload.route");
@@ -39,7 +47,60 @@ mongoose
     console.log(err);
   });
 
+// Socket Configuration
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  console.log("A new user connected with id : " + socket.id);
+
+  socket.on("addUser", (userId) => {
+    console.log("userId => ", userId);
+    console.log("socket.id => ", socket.id);
+    addUser(userId, socket.id);
+    // Send the users array to the client
+    io.emit("getUsers", users);
+  });
+
+  // Get the message from the client and Send to specific user/receiver
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    console.log("recieverId => ", receiverId);
+    console.log("senderId => ", senderId);
+    console.log("text => ", text);
+    const user = getUser(receiverId); // Get the receiver
+    // Send a message to a specific User -> socket.to("socketId").emit()
+    if (user) {
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+      });
+    } else {
+      console.log("User not online!");
+    }
+  });
+
+  // Disconnect socket server after user disconnects (moved away from conversation)
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
+
 // Listen on port 5000
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server started on port 5000");
+server.listen(process.env.PORT || 5000, () => {
+  console.log("Converse Server Started on PORT : " + process.env.PORT);
 });
